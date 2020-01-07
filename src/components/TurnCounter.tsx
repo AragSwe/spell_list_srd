d01728ddf527cf37b46cef32818a1f97d3c12938
Mikael Gullberg <zeractor@gmail.com>
2020-01-07 20:22:44 +0100

    spell duration countdown

--- src/components/TurnCounter.tsx
+++ src/components/TurnCounter.tsx
@@ -1,15 +1,17 @@
 import React, { useState } from "react"
 import { observer } from "mobx-react"
-//import { useStores } from "../hooks/use-stores";
+import { useStores } from "../hooks/use-stores";
 import { Button } from "./Button";
+import { SpellListItem } from "./SpellListItem";
 
 export const TurnCounter = observer(() => {
-   //const { spellStore } = useStores();
+    const { spellStore } = useStores();
     const [turnCounter, setTurnCounter] = useState(1);
 
     const onAdvanceTurnCounter = (e: React.FormEvent) => { 
         e.preventDefault();
         setTurnCounter(turnCounter + 1);
+        spellStore.countDownSpellDuration();
     }
 
     const onResetTurnCounter = (e: React.FormEvent) => { 
@@ -21,6 +23,14 @@ export const TurnCounter = observer(() => {
         <div>
             <h2>Turns</h2>
             <p>Current turn: {turnCounter}</p>
+            <p>Active spells:</p>
+            {   (spellStore.getActiveSpells.length > 0 &&
+                <ul>
+                    {spellStore.getActiveSpells.map(spell =>
+                        <li><SpellListItem spell={spell}/></li>)}
+                </ul>)
+                || <p>None</p>
+            }
             <Button caption="Next turn" onClick={onAdvanceTurnCounter} />
             <Button caption="Rest turns" onClick={onResetTurnCounter} />
         </div>
--- src/stores/spell-store.tsx
+++ src/stores/spell-store.tsx
@@ -5,26 +5,35 @@ export type TSpell = {
   isUsed: boolean,
   level: number,
   duration: number | null,
+  maxDuration: number | null,
 }
 
 export class SpellStore {
-  @observable knownSpells = [{name:'flamestrike!', level:3, duration:null, isUsed:false}] as TSpell[]
+  @observable knownSpells = [{name:'flamestrike!', level:3, duration:null, isUsed:false, maxDuration: null}] as TSpell[]
 
   @action
   addKnownSpell(name: string, level: number, duration: number | null) {
-    this.knownSpells.push({name, level, duration, isUsed: false });
+    this.knownSpells.push({name, level, duration: null, isUsed: false, maxDuration: duration });
   }
 
   @action
   castKnownSpell(name: string) {
     var knownSpell = this.knownSpells.find(spell => spell.name === name);
-    if (knownSpell)
+    if (knownSpell) {
       knownSpell.isUsed = true;
+      knownSpell.duration = knownSpell.maxDuration;
+    }
   }
 
   @action
   refreshSpells() {
     this.knownSpells.forEach(s => s.isUsed = false);
+  }
+
+  @action
+  countDownSpellDuration() {
+    const activeSpells = this.getActiveSpells;
+    activeSpells.forEach(spell => spell.duration && spell.duration--);
   }
 
   @computed
@@ -35,5 +44,10 @@ export class SpellStore {
   @computed
   get getUsedSpells() {
     return this.knownSpells.filter(spell => spell.isUsed)
+  }
+
+  @computed
+  get getActiveSpells() {
+    return this.knownSpells.filter(spell => spell.isUsed && (spell.duration ?? 0) > 0)
   }
 }
