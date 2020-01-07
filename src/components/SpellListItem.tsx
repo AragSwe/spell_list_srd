54f4f8040ab72a100a0d1f8cf449d65fb7cddc1e
Mikael Gullberg <zeractor@gmail.com>
2020-01-07 20:27:01 +0100

    fixed initial duration visibility  on spellistitem

--- src/components/SpellListItem.tsx
+++ src/components/SpellListItem.tsx
@@ -3,6 +3,6 @@ import { TSpell } from "../stores/spell-store";
 
 export const SpellListItem = (props: { spell: TSpell }) => {
     return <label>
-        Level {props.spell.level} { props.spell.name } { props.spell.duration && <span>{ props.spell.duration } </span>}
+        Level {props.spell.level} { props.spell.name } { props.spell.maxDuration && <span>{ props.spell.maxDuration } </span>}
     </label>
 }
